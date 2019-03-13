a050bc66f15e78a1ab9a0c0da0bef670493f6c24
felperez <fel.prz@gmail.com>
2019-03-13 12:59:09 -0500

    first version of the file

---
+++ main/main.ino
@@ -0,0 +1,14 @@
+// include the library code:
+#include <LiquidCrystal.h>
+#include <pitches.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+void setup() {
+
+}
+
+void loop() {
+
+}
